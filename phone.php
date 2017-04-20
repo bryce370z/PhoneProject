2cc088aa346dc36a95844d24bd1a5b7927126683
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2017-04-20 16:56:06 -0500

    phone almost done

--- phone.php
+++ phone.php
@@ -8,7 +8,14 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class = "row">
   <div class="container col-xs-3">
-    <h1>Directory</h1>
+    <div class = "row">
+      <div class = "container col-xs-1">
+        <a class="btn btn-large btn-info" href="main.php">Home</a>
+      </div>
+      <div class = "container col-xs-3 col-xs-offset-2" style = "padding-bottom: 3%">
+    <h3>Directory</h3>
+  </div>
+  </div>
     <div class= "table-responsive" style="height: 75%; overflow:auto">
       <table class="table table-hover">
         <thead>
@@ -88,13 +95,59 @@
       </table>
     </div>
   </div>
-  <div class= "container col-xs-9">
-    <h1> Phone Interface </h1>
-    <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" style = "width: 75%;">
-    <div class = "row" style= "padding-top: 5%;">
+  <div class= "container col-xs-9" style="padding-left: 10%">
+    <div class = "row" style= "padding-bottom: 3%">
+    <div class = "containter col-xs-3 col-xs-offset-3">
+    <h3 style= "text-align:center"> Phone </h3>
+  </div>
+</div>
+    <input type="text" class="form-control" placeholder="Number will be displayed here" aria-describedby="basic-addon1" style = "width: 70%; height:10%">
+    <div class = "row" style= "padding-top: 3%;">
       <div class = "containter col-xs-3">
         <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">1</button>
       </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">2</button>
+      </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">3</button>
+      </div>
+    </div>
+
+    <div class = "row" style= "padding-top: 3%;">
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">4</button>
+      </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">5</button>
+      </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">6</button>
+      </div>
+    </div>
+
+    <div class = "row" style= "padding-top: 3%;">
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">7</button>
+      </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">8</button>
+      </div>
+      <div class = "containter col-xs-3">
+        <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">9</button>
+      </div>
+    </div>
+    <div class = "row" style = "padding-top: 3%;">
+      <div class = "container col-xs-9">
+        <button style="height: 10%; width:94%;" type="button" class= "btn btn-large btn-info">0</button>
+      </div>
+  </div>
+  <div class = "row" style = "padding-top: 3%;">
+    <div class ="container col-xs-3">
+      <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">Call</button>
+    </div>
+    <div class ="container col-xs-3 col-xs-offset-3">
+      <button style= "height: 10%; width: 80%;" type="button" class="btn btn-large btn-info">End</button>
     </div>
   </div>
 </div>
