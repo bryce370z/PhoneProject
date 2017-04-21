098084e06522e3e81dabb0675a3e4f9b3d0b04a5
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2017-04-20 22:43:00 -0500

    added manual page

--- manual.php
+++ manual.php
@@ -6,3 +6,51 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<div class = "row">
+  <div class = "container col-xs-12">
+    <div class = "container col-xs-1">
+      <a class="btn btn-large btn-info" href="main.php">Home</a>
+    </div>
+  </div>
+</div>
+<div class = "row">
+  <div class = "container col-xs-12">
+    <div class = "container col-xs-4">
+      <h1 style= "text-align: center;"> Call Instructions </h1>
+      <div class = "row">
+        <div class = "container col-xs-12">
+          <ul style="list-style-type:disc">
+            <li><p>To make a call input the recipients number on the numberpad, and press the "Call" Button.</p></li>
+            <li><p>To end end a current call or reject an incoming one, press the "End" Button.</p></li>
+            <li><p>To redial the last number dialed, press the "Redial" Button.</p></li>
+            <li><p>To view recent call history, press the "Call History" Button.</p></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class = "container col-xs-4">
+      <h1 style= "text-align: center;"> Text Instructions </h1>
+      <div class = "row">
+        <div class = "container col-xs-12">
+          <ul style="list-style-type:disc">
+            <li><p>To send a text message type the message in the lower box, and press the "Send" button.</p></li>
+            <li><p>The current conversation will appear in the upper box.</p></li>
+            <li><p>To view recent text message history press the "Text History" button.</p></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class = "container col-xs-4">
+      <h1 style= "text-align: center;"> Settings Instructions </h1>
+      <div class = "row">
+        <div class = "container col-xs-12">
+          <ul style="list-style-type:disc">
+            <li><p>To block someone from being able to call or text you, input their name or number in the box provided, and press the "Block" button.</p></li>
+            <li><p>To unblock a person who is currently blocked, type their name or number in the box provided, and press the "Unblock" button.</p></li>
+            <li><p>To add someone to the directory of known recipients, type their name or number in the box prvovided, and press the "Add" button.</p></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
